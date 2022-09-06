7d0040d5f33d4fe5508cfb03021973b19df9b80b
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-06 16:20:36 +0200

    Initial commit

---
+++ src/main/java/com/example/Spring/REST/API/projekt/SpringRestApiProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.REST.API.projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiProjektApplication.class, args);
+	}
+
+}
