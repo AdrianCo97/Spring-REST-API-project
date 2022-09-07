125d7e14e82faa072703ec1813620af7504c797c
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-07 13:19:38 +0200

    Created a postmappin for creating a user

--- src/main/java/com/example/Spring/REST/API/projekt/user/UserController.java
+++ src/main/java/com/example/Spring/REST/API/projekt/user/UserController.java
@@ -1,8 +1,16 @@
 package com.example.Spring.REST.API.projekt.user;
 
+import org.hibernate.PropertyValueException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-@RestController("/api/user")
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -11,5 +19,14 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    
+    @PostMapping("/")
+    public ResponseEntity<String> newUser(@RequestBody User user) {
+        try{
+            userRepository.save(user);
+            return new ResponseEntity<>("User saved", HttpStatus.OK);
+        }
+        catch (Exception exception){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User wasn't created", exception);
+        }
+    }
 }
