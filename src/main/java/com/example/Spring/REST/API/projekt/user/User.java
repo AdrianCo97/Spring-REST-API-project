0173607c8102703e1ee401bf8e313ffa078838ae
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-07 11:18:58 +0200

    Added the notnull annotation to make sure that any field is not null

--- src/main/java/com/example/Spring/REST/API/projekt/user/User.java
+++ src/main/java/com/example/Spring/REST/API/projekt/user/User.java
@@ -1,5 +1,7 @@
 package com.example.Spring.REST.API.projekt.user;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,8 +13,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    @NotNull
     private String firstName;
+
+    @NotNull
     private String lastName;
+
+    @NotNull
     private String email;
 
     public User(String firstName, String lastName, String email) {
