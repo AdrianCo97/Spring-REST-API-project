8a03983f4f8c41a24ff092e0bff1844c060cd36c
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-07 11:12:27 +0200

    Setup the basics of the usercontroller

---
+++ src/main/java/com/example/Spring/REST/API/projekt/user/UserController.java
@@ -0,0 +1,11 @@
+package com.example.Spring.REST.API.projekt.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/user")
+public class UserController {
+
+}
