2e9a48393fe5e09deef24c498876aa83b240712c
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-15 13:14:56 +0200

    Created an ErrorHandler class

---
+++ src/main/java/com/example/Spring/REST/API/projekt/user/errorhandling/ErrorHandler.java
@@ -0,0 +1,33 @@
+package com.example.Spring.REST.API.projekt.user.errorhandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    /*
+      This method is responsible for handling errors thrown by the Validation package.
+      And then sending back detailed error messages in the response.
+    */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message =        error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return errors;
+    }
+
+}
