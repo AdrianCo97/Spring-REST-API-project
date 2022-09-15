860215cb02155a7fcdc78442eb32160990335bb7
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-15 14:57:34 +0200

    Created a getmapping for finding a user by id

--- src/main/java/com/example/Spring/REST/API/projekt/user/controller/UserController.java
+++ src/main/java/com/example/Spring/REST/API/projekt/user/controller/UserController.java
@@ -2,15 +2,19 @@ package com.example.Spring.REST.API.projekt.user.controller;
 import com.example.Spring.REST.API.projekt.user.model.UserModel;
 import com.example.Spring.REST.API.projekt.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -23,25 +27,26 @@ public class UserController {
     }
 
     @PostMapping("/")
-    public UserModel createUser(@Valid @RequestBody UserModel userModel){
+    public UserModel createUser(@Valid @RequestBody UserModel userModel) {
         userRepository.save(userModel);
         return userModel;
     }
 
     @GetMapping("/allUsers")
     public List<UserModel> getUsers(){
-        return (List)userRepository.findAll();
+        List<UserModel> allUsers = (List)userRepository.findAll();
+        return allUsers;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception) {
-        Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String message = error.getDefaultMessage();
-            errors.put(fieldName, message);
-        });
-        return errors;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserModel> findUser(@PathVariable Long id){
+        Optional<UserModel> foundUser = Optional.ofNullable(userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException()));
+
+
+
+        return new ResponseEntity(foundUser, HttpStatus.OK);
     }
+
 }
