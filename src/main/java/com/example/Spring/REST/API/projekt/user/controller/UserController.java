1c20e96e3877c8b142968263cfc95a0907607177
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-15 12:06:08 +0200

    Update UserController.java

--- src/main/java/com/example/Spring/REST/API/projekt/user/controller/UserController.java
+++ src/main/java/com/example/Spring/REST/API/projekt/user/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.example.Spring.REST.API.projekt.user.controller;
-
 import com.example.Spring.REST.API.projekt.user.model.UserModel;
 import com.example.Spring.REST.API.projekt.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,5 +31,17 @@ public class UserController {
     @GetMapping("/allUsers")
     public List<UserModel> getUsers(){
         return (List)userRepository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return errors;
     }
 }
